44fb03d3234e55fe583b9147446dbe0034ff63be
João Pedro Garcia <joao.garcia.bolsista@icmbio.gov.br>
2025-09-30 16:40:47 -0300

    install and setting phpunit

---
+++ package/tests/Unit/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Icmbio\Xform\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testTrueAssertsToTrue(): void
+    {
+        $this->assertTrue(true);
+    }
+}
