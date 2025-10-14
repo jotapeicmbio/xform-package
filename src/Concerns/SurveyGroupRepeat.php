23e3d63908fef6241e629802ca29c30571ece418
João Pedro Garcia <joao.garcia.bolsista@icmbio.gov.br>
2025-10-14 14:34:57 -0300

    get specific group repeats

--- src/Concerns/SurveyGroupRepeat.php
+++ src/Concerns/SurveyGroupRepeat.php
@@ -20,6 +20,12 @@ trait SurveyGroupRepeat
         return array_map(fn($n) => $n->nodeValue, iterator_to_array($nodes));
     }
 
+    public function getSpecificGroupRepeats(): array
+    {
+        $nodes = $this->xpath()->query("//x:repeat[contains(@nodeset, '{$this->group_repeat}')]/@nodeset");
+        return array_map(fn($n) => $n->nodeValue, iterator_to_array($nodes));
+    }
+
     public function getGroupRepeatsWithUuid(): array
     {
         $nodes = $this->xpath()->query("//x:bind[contains(@nodeset, 'uuid') and contains(@nodeset, '{$this->group_repeat}')]/@nodeset");
